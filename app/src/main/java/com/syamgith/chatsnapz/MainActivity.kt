125570ef881e9d8f69da0f5668b3d7e96cadae4b
syamgith <syamgithksudheesh@gmail.com>
2020-04-16 21:03:27 +0530

    Initial commit

---
+++ app/src/main/java/com/syamgith/chatsnapz/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.syamgith.chatsnapz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
