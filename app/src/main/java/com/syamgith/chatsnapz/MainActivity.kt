560413ccdb404652dfe0da262399d57814a3c68e
syamgith <syamgithksudheesh@gmail.com>
2020-04-17 00:00:26 +0530

    initial3

--- app/src/main/java/com/syamgith/chatsnapz/MainActivity.kt
+++ app/src/main/java/com/syamgith/chatsnapz/MainActivity.kt
@@ -2,11 +2,54 @@ package com.syamgith.chatsnapz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    var emailEditText: EditText? = null
+    var passewordEditText: EditText? = null
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passewordEditText = findViewById(R.id.passwordEditText)
+
+        if (mAuth != null) {
+            logIn()
+        }
+
     }
+
+    fun goClicked(view: View) {
+        //check if we can login the user
+        mAuth.signInWithEmailAndPassword(emailEditText?.text.toString(), emailEditText?.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    logIn()
+                } else {
+                    //Sign  up the user
+                    mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(), emailEditText?.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                //add to data base
+                                logIn()
+                            } else {
+                                Toast.makeText(this,"Login failed, try again.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                }
+            }
+    }
+
+    fun logIn() {
+        //move to next activity
+    }
+
 }
---
+++ app/src/main/java/com/syamgith/chatsnapz/SnapsActivity.kt
@@ -0,0 +1,12 @@
+package com.syamgith.chatsnapz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snaps)
+    }
+}
