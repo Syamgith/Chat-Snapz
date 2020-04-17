5a95256ac627486e0ebde9cd5e862a0a7943fcb4
syamgith <syamgithksudheesh@gmail.com>
2020-04-17 13:23:42 +0530

    url of image is returned

--- app/src/main/java/com/syamgith/chatsnapz/CreateSnapActivity.kt
+++ app/src/main/java/com/syamgith/chatsnapz/CreateSnapActivity.kt
@@ -3,19 +3,34 @@ package com.syamgith.chatsnapz
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.util.*
+
 
 class CreateSnapActivity : AppCompatActivity() {
 
     var createSnapImageView: ImageView? = null
     var messageEditText: EditText? = null
+    val imageName = UUID.randomUUID().toString() + ".jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snap)
+        createSnapImageView = findViewById(R.id.createSnapImageView)
+        messageEditText = findViewById(R.id.messageEditText)
     }
 
     fun getPhoto() {
@@ -24,7 +39,7 @@ class CreateSnapActivity : AppCompatActivity() {
         startActivityForResult(intent, 1)
     }
 
-    fun chooseImageClicked() {
+    fun chooseImageClicked(view: View) {
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED) {
             requestPermissions(
                 arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -60,5 +75,33 @@ class CreateSnapActivity : AppCompatActivity() {
                 getPhoto()
             }
         }
+    }
+
+    fun nextClicked(view: View) {
+        // Get the data from an ImageView as bytes
+        createSnapImageView?.isDrawingCacheEnabled = true
+        createSnapImageView?.buildDrawingCache()
+        val bitmap = (createSnapImageView?.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+
+        var uploadTask = FirebaseStorage.getInstance().getReference().child("images").child(imageName).putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+            Toast.makeText(this,"Upload failed",Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener {taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            val urlTask: Task<Uri> = taskSnapshot.storage.downloadUrl
+            while (!urlTask.isSuccessful());
+            if (urlTask.isSuccessful) {
+                val downloadURL = urlTask.getResult()
+
+                Log.i("url", downloadURL.toString())
+            }
+            Log.i("URL","Done")
+        }
+
     }
 }
